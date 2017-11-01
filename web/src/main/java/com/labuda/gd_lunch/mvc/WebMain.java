b48e05a75d64ae6de3f3fb05bbe6e3a593b01db7
Dominik Labuda <dominikla@gmail.com>
2017-11-01 17:04:34 +0100

    Spring Boot added to project

---
+++ web/src/main/java/com/labuda/gd_lunch/mvc/WebMain.java
@@ -0,0 +1,29 @@
+package com.labuda.gd_lunch.mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main starting class
+ */
+@SpringBootApplication
+public class WebMain {
+
+    /**
+     * Logger
+     */
+    private final static Logger log = LoggerFactory.getLogger(WebMain.class);
+
+    /**
+     * Main method
+     *
+     * @param args command line args
+     */
+    public static void main(String... args) {
+        log.info("Starting the web application");
+        SpringApplication.run(WebMain.class, args);
+    }
+
+}
